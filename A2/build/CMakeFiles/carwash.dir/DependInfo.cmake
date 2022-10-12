d3471207538bb80221528989bc7ef52b9c9fb46a
Nemanja Bulatović <bulatovic@student.tugraz.at>
2022-10-12 22:31:27 +0200

    added second folder

---
+++ A2/build/CMakeFiles/carwash.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nemanja/Desktop/SLP/slpw22g203/A2/carwash.c" "/home/nemanja/Desktop/SLP/slpw22g203/A2/build/CMakeFiles/carwash.dir/carwash.c.o"
+  "/home/nemanja/Desktop/SLP/slpw22g203/A2/vector.c" "/home/nemanja/Desktop/SLP/slpw22g203/A2/build/CMakeFiles/carwash.dir/vector.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
